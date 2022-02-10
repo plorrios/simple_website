8e1029e5f9f0900cd4592982cb81232e33f891c0
Plorrios <pablolopezorrios@hotmail.es>
2022-02-10 13:49:47 +0100

    first commit

---
+++ web.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+Pagina de prueba para COS. NFS
+<?php
+echo “Hoy es “.date(“d/m/y”).“<br>”;
+echo “Son las “.date(“h:i:s”).“<br>”;
+echo “Soy el servidor “.$_SERVER[‘SERVER_ADDR’].“<br>”;  
+?>
+</body>
+</html>
